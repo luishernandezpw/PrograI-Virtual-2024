f3d2bc53fd049cb98354a228dcddd5feb6c12604
Luis Hernandez <sarco_luis@hotmail.com>
2024-07-16 20:18:39 -0600

    Ejercicio de suma de dos numeros

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -3,7 +3,13 @@
 namespace miPrimerProyecto {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hola Mundo");
+            //Ejercicio de suma de dos numeros.
+            int num1 = 10;
+            int num2 = 5;
+
+            int res = num1 + num2;
+
+            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, res);
 
             Console.ReadLine();
         }

2f2589168ee53722cbe250d8b02d7a3e1d5c9faa
Luis Hernandez <sarco_luis@hotmail.com>
2024-07-16 20:38:09 -0600

    Ejercicio de pedir el nombre y la edad y dar un saludo

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -3,16 +3,15 @@
 namespace miPrimerProyecto {
     internal class Program {
         static void Main(string[] args) {
-            //Ejercicio de suma de dos numeros iontroducidos por el usuario.
-            Console.Write("Num 1: ");
-            int num1 = int.Parse(Console.ReadLine()); 
+            //Ejercicio de pedir el nombre y la edad y dar un saludo
+            Console.Write("Nombre: ");
+            String nombre = Console.ReadLine();
 
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Edad: ");
+            int edad = int.Parse(Console.ReadLine());
 
-            int res = num1 + num2;
-
-            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, res);
+            Console.WriteLine("Hola, {0} bienvenido a Progra I, estas en la mejor epoca {1} de aprender a programar.",
+                nombre, edad);
 
             Console.ReadLine();
         }

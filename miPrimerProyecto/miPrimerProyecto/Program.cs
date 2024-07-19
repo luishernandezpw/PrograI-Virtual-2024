94531366bff1437366faea75375095e3a1c26af3
Luis Hernandez <sarco_luis@hotmail.com>
2024-07-18 19:12:10 -0600

    Uso de la estructura de control while

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -4,16 +4,19 @@ namespace miPrimerProyecto {
     internal class Program {
         static void Main(string[] args) {
             //Ejercicio de pedir el nombre y la edad y dar un saludo
-            Console.Write("Nombre: ");
-            String nombre = Console.ReadLine();
+            string continuar = "s";
+            while (continuar == "s") {
+                Console.Write("Nombre: ");
+                String nombre = Console.ReadLine();
 
-            Console.Write("Edad: ");
-            int edad = int.Parse(Console.ReadLine());
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Hola, {0} bienvenido a Progra I, estas en la mejor epoca {1} de aprender a programar.",
-                nombre, edad);
-
-            Console.ReadLine();
+                Console.WriteLine("Hola, {0} bienvenido a Progra I, estas en la mejor epoca {1} de aprender a programar.",
+                    nombre, edad);
+                Console.Write("Continuar = s; Salir = cualquier tecla");
+                continuar = Console.ReadLine();
+            }
         }
     }
 }

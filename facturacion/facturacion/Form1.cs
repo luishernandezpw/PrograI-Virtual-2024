f544d014f98da730124d73ccbe41a19a42a56612
Luis Hernandez <sarco_luis@hotmail.com>
2024-07-25 20:30:48 -0600

    Terminacion del CRUD (Crear, Leer, Actualizar, Eliminar) de clientes

--- facturacion/facturacion/Form1.cs
+++ facturacion/facturacion/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Creado Por: 
+//LUIS ENRIQUE HERNANDEZ SANCHEZ.
 namespace facturacion {
     public partial class Form1 : Form {
         public Form1() {
@@ -56,6 +58,56 @@ namespace facturacion {
         private void btnUltimo_Click(object sender, EventArgs e) {
             clientesBindingSource.MoveLast();
             registros();
+        }
+        private void estadoCtrls(Boolean estado) {
+            grbNavegacion.Enabled = estado;
+            btnEliminar.Enabled = estado;
+            grbDatos.Enabled = !estado;
+            registros();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e) {
+            if( btnAgregar.Text=="Agregar") {
+                btnAgregar.Text = "Guardar";
+                btnModificar.Text = "Cancelar";
+                estadoCtrls(false);
+                clientesBindingSource.AddNew();//Agrega un nuevo registro.
+            } else {//guardar
+
+                clientesBindingSource.EndEdit();
+                clientesTableAdapter.Update(this.db_facturacionDataSet);
+                estadoCtrls(true);
+                btnAgregar.Text = "Agregar";
+                btnModificar.Text = "Modificar";
+            }
+        }
+        private void btnModificar_Click(object sender, EventArgs e) {
+            if (btnModificar.Text == "Modificar") {
+                btnAgregar.Text = "Guardar";
+                btnModificar.Text = "Cancelar";
+                estadoCtrls(false);
+
+            } else {//Cancelar
+                clientesBindingSource.CancelEdit();
+                estadoCtrls(true);
+                btnAgregar.Text = "Agregar";
+                btnModificar.Text = "Modificar";
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e) {
+            if(MessageBox.Show("Esta seguro de eliminar a "+ nombreTextBox.Text.Trim()+"?", "Eliminando registro clientes", MessageBoxButtons.YesNo, MessageBoxIcon.Question)==DialogResult.Yes){
+                clientesBindingSource.RemoveCurrent();
+                clientesTableAdapter.Update(db_facturacionDataSet);
+                registros();
+            }
+        }
+
+        private void txtbuscar_TextChanged(object sender, EventArgs e) {
+            BindingSource bs = new BindingSource();
+            bs.DataSource = clientesDataGridView.DataSource;
+            bs.Filter = "codigo = '"+ txtbuscar.Text +"' or nombre like '%"+ txtbuscar.Text +"%' or dui like '%"+ txtbuscar.Text +"%'";
+            clientesDataGridView.DataSource = bs;
         }
     }
 }

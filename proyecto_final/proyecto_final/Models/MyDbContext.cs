79b88f7009e0fe73a1b327e7f42188c6fe8ef07d
Luis Hernandez <sarco_luis@hotmail.com>
2024-08-13 21:09:27 -0600

    Creacion del proyecto de API con ASP.NET Core con Entity Framework

---
+++ proyecto_final/proyecto_final/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace proyecto_final.Models {
+    public class MyDbContext : DbContext{
+        public MyDbContext() { }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Cliente>().HasKey(e => e.idCliente);
+        }
+
+    }
+}
